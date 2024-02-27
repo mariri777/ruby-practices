df3738953116532290d41e398f1a65dcc190f4dd
mariri <rmrmrm7rmrmrm@gmail.com>
2024-02-27 23:06:52 +0900

    カレンダーのプラクティスを提出

---
+++ 02.calendar/cal.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'date'
+
+options = { year: Date.today.year, month: Date.today.month }
+opt = OptionParser.new
+
+opt.on('-y YEAR', Integer) do |year|
+  if (1970..2100).cover?(year)
+    options[:year] = year
+  else
+    puts "指定範囲外です"
+  end
+end
+
+opt.on('-m MONTH', Integer) do |month|
+  if (1..12).cover?(month)
+    options[:month] = month
+  else
+    puts "指定範囲外です"
+  end
+end
+
+opt.parse!(ARGV)
+
+date = Date.new(options[:year], options[:month], 1)
+first_day = Date.new(options[:year], options[:month], 1)
+last_day = Date.new(options[:year], options[:month], -1)
+
+puts "      #{options[:month]}月 #{options[:year]}"
+puts "日 月 火 水 木 金 土"
+
+first_day.wday.times { print "   " }
+
+(first_day..last_day).each do |everyday|
+  print format("%2d ", everyday.day)
+  if everyday.wday == 6
+    puts "\n"
+  end
+end

23e3f3c2a96b44c3c8e8cf6b878bc8e1a0c861f8
mariri <rmrmrm7rmrmrm@gmail.com>
2024-03-04 21:48:35 +0900

    2回目の提出
    1.月指定、年指定の文言を変更
    2.`rjust`を使用し文字列を整形
    3.`saturday?`メソッドを使用
    4.引数なしで改行を出力
    5.出力の最後に改行を追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -10,7 +10,7 @@ opt.on('-y YEAR', Integer) do |year|
   if (1970..2100).cover?(year)
     options[:year] = year
   else
-    puts "指定範囲外です"
+    puts "年の指定が範囲外です"
   end
 end
 
@@ -18,7 +18,7 @@ opt.on('-m MONTH', Integer) do |month|
   if (1..12).cover?(month)
     options[:month] = month
   else
-    puts "指定範囲外です"
+    puts "月の指定が範囲外です"
   end
 end
 
@@ -30,11 +30,13 @@ last_day = Date.new(options[:year], options[:month], -1)
 puts "      #{options[:month]}月 #{options[:year]}"
 puts "日 月 火 水 木 金 土"
 
-first_day.wday.times { print "   " }
+first_day.wday.times { print "   "}
 
 (first_day..last_day).each do |date|
-  print format("%2d ", date.day)
-  if date.wday == 6
-    puts ""
+  print date.day.to_s.rjust(3)
+  if date.saturday?
+    puts
   end
 end
+
+puts
